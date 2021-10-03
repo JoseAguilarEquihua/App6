4e4c1460b8025e18aef8647e116c82732ccad33a
JoseAguilarEquihua <joseequihua95@gmail.com>
2021-10-03 03:06:14 -0500

    Agregar archivos de proyecto.

---
+++ App6/App6/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,69 @@
+﻿using App6.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+
+namespace App6.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        Command _goToViewStartCommand;
+        private string _usuario;
+        private string _contrasena;
+        private bool isError;
+
+        public MainPageViewModel(INavigation navigation) : base(navigation)
+        {
+        }
+
+        public string Usuario
+        {
+            get => _usuario;
+            set
+            {                
+                _usuario = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Contrasena
+        {
+            get => _contrasena;
+            set
+            {                
+                _contrasena = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Command GoToViewStartCommand
+        {
+            get => _goToViewStartCommand ?? (_goToViewStartCommand = new Command(GoToViewAction));
+        }
+
+        public bool IsError
+        {
+            get => isError;
+            set
+            {
+                isError = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void GoToViewAction()
+        {     
+            IsError = !(string.Equals(Usuario, "hola") && string.Equals(Contrasena, "1234"));
+            
+            if(!IsError)
+            {                
+                Usuario = string.Empty;
+                Contrasena = string.Empty;
+                Navigation.PushAsync(new NavigationPage(new PageStart()));
+            }
+            
+        }
+    }
+}
---
+++ App6/App6/ViewModels/PageStartViewModel.cs
@@ -0,0 +1,46 @@
+﻿using App6.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App6.ViewModels
+{
+    public class PageStartViewModel : BaseViewModel
+    {
+        Command _goToLogin;
+        Command _goToViewProfileCommand;
+
+        public PageStartViewModel(INavigation navigation = null) : base(navigation)
+        {
+            Navigation = navigation ?? App.Navigation;
+        }
+
+        public Command GoToLoginCommand
+        {
+            get
+            {
+                if (_goToLogin == null)
+                {
+                    _goToLogin = new Command(GoToLoginAction);
+                }
+                return _goToLogin;
+            }
+        }
+
+        private void GoToLoginAction()
+        {
+            Navigation.PopToRootAsync();
+        }
+
+        public Command GoToViewProfileCommand
+        {
+            get => _goToViewProfileCommand ?? (_goToViewProfileCommand = new Command(GoToViewAction));
+        }
+
+        private void GoToViewAction()
+        {           
+                Navigation.PushAsync(new NavigationPage(new PageProfile()));
+        }
+    }
+}
